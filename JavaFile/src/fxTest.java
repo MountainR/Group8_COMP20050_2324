24bfdd8913e34ffc0d46a46798a3887ba3735e2d
Labiba Jaigirdar <labiba.jaigirdar@ucdconnect.ie>
2024-02-15 16:58:22 +0000

    update

--- JavaFile/src/fxTest.java
+++ JavaFile/src/fxTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class fxTest {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class fxTest {
+
+    public static void test() {
+        Stage stage = new Stage();
+        stage.setTitle("Board Display");
+        stage.show();
+    }
+
 }
