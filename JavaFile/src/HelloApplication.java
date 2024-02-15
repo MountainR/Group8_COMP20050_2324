8193e410b6dab9abb7eb1d283f824d2833759ba8
Labiba Jaigirdar <labiba.jaigirdar@ucdconnect.ie>
2024-02-15 18:36:08 +0000

    Changed background colour to black

--- JavaFile/src/HelloApplication.java
+++ JavaFile/src/HelloApplication.java
@@ -26,29 +26,32 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
 
+        // just some text
         Pane row1 = createRow("1", "2", "3", "4", "5");
         Pane row2 = createRow("1", "2", "3", "4", "5", "6");
         Pane row3 = createRow("1", "2", "3", "4", "5", "6", "7");
         Pane row4 = createRow("1", "2", "3", "4", "5", "6", "7", "8");
-        Pane row5 = createRow("1", "2", "3", "4", "5", "6", "7");
-        Pane row6 = createRow("1", "2", "3", "4", "5", "6");
-        Pane row7 = createRow("1", "2", "3", "4", "5");
+        Pane row5 = createRow("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        Pane row6 = createRow("1", "2", "3", "4", "5", "6", "7", "8");
+        Pane row7 = createRow("1", "2", "3", "4", "5", "6", "7");
+        Pane row8 = createRow("1", "2", "3", "4", "5", "6");
+        Pane row9 = createRow("1", "2", "3", "4", "5");
 
 
-        VBox pane = new VBox(row1, row2, row3, row4, row5, row6, row7);
-        pane.setAlignment(Pos.CENTER);
-        VBox.setMargin(row1, new Insets(-10, -10, -10, -10));
+        VBox pane = new VBox(row1, row2, row3, row4, row5, row6, row7, row8, row9);
+        VBox.setMargin(row1, new Insets(-29, -29, -29, -29));
         VBox.setMargin(row2, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row3, new Insets(-10, -10, -10, -10));
+        VBox.setMargin(row3, new Insets(-29, -29, -29, -29));
         VBox.setMargin(row4, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row5, new Insets(-10, -10, -10, -10));
+        VBox.setMargin(row5, new Insets(-29, -29, -29, -29));
         VBox.setMargin(row6, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row7, new Insets(-10, -10, -10, -10));
+        VBox.setMargin(row7, new Insets(-29, -29, -29, -29));
+        VBox.setMargin(row8, new Insets(0, 0, 0, 0));
+        VBox.setMargin(row9, new Insets(-29, -29, -29, -29));
 
 
-
-
-        pane.setStyle("-fx-background-color: #37474f; -fx-padding: 20;");
+        pane.setAlignment(Pos.CENTER);
+        pane.setStyle("-fx-background-color: #000000; -fx-padding: 20;");
 
         Scene scene = new Scene(pane);
 
@@ -67,22 +70,24 @@ public class HelloApplication extends Application {
     }
 
     private static Pane createStar(String text) {
-        Polygon star = new Polygon(0, 10, 17, 0,
-                34, 10, 34, 30,
-                17, 40, 0, 30,
-                0, 10);
+        int halfEdge = 25;
+        Polygon star = new Polygon(0, halfEdge, 1.73*halfEdge, 0,
+                3.46*halfEdge, halfEdge, 3.46*halfEdge, 3*halfEdge,
+                1.73*halfEdge, 4*halfEdge, 0, 3*halfEdge,
+                0, halfEdge
+        );
 
         star.setFill(Color.TRANSPARENT);
         star.setStrokeWidth(2);
-        star.setStroke(Color.BLACK);
+        star.setStroke(Color.WHITE);
 
         // Removes the small gap between rows
-        star.setScaleX(1.1);
-        star.setScaleY(1.1);
+        star.setScaleX(1.04);
+        star.setScaleY(1.0);
 
         Label label = new Label(text);
         label.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-        label.setTextFill(Color.WHITE);
+        label.setTextFill(Color.BLACK);
 
         StackPane pane = new StackPane();
         pane.getChildren().addAll(star, label);
