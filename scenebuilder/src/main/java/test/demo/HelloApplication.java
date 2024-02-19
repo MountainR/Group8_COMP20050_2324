3f95bbcb77986dc8dbadf8a9e87db2102fee0435
Labiba Jaigirdar <labiba.jaigirdar@ucdconnect.ie>
2024-02-19 11:01:24 +0000

    gui made in scenebuilder

--- scenebuilder/src/main/java/test/demo/HelloApplication.java
+++ scenebuilder/src/main/java/test/demo/HelloApplication.java
@@ -1,9 +1,9 @@
 package test.demo;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -11,12 +11,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,9 +20,11 @@ import java.util.Arrays;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
         // just some text
+        Pane row0 = createTextRow(5);
         Pane row1 = createRow(5);
         Pane row2 = createRow(6);
         Pane row3 = createRow(7);
@@ -38,22 +36,22 @@ public class HelloApplication extends Application {
         Pane row9 = createRow(5);
 
 
-        VBox pane = new VBox(row1, row2, row3, row4, row5, row6, row7, row8, row9);
-        VBox.setMargin(row1, new Insets(-26, -26, -26, -26));
-        VBox.setMargin(row2, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row3, new Insets(-26, -26, -26, -26));
-        VBox.setMargin(row4, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row5, new Insets(-26, -26, -26, -26));
-        VBox.setMargin(row6, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row7, new Insets(-26, -26, -26, -26));
-        VBox.setMargin(row8, new Insets(0, 0, 0, 0));
-        VBox.setMargin(row9, new Insets(-26, -26, -26, -26));
+        VBox pane = new VBox(row0, row1, row2, row3, row4, row5, row6, row7, row8, row9);
+        VBox.setMargin(row1, new Insets(0, 0, 0, 0));
+        VBox.setMargin(row2, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row3, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row4, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row5, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row6, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row7, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row8, new Insets(-26, 0, 0, 0));
+        VBox.setMargin(row9, new Insets(-26, 0, 0, 0));
 
 
         pane.setAlignment(Pos.CENTER);
         pane.setStyle("-fx-background-color: #000000; -fx-padding: 20;");
 
-        Scene scene = new Scene(pane);
+        Scene scene = new Scene(fxmlLoader.load());
 
         stage.setScene(scene);
         stage.show();
@@ -64,7 +62,21 @@ public class HelloApplication extends Application {
         pane.setAlignment(Pos.CENTER);
 
         for (int i = 0; i < n; i++) {
-            pane.getChildren().add(createStar());
+            pane.getChildren().addAll(createStar());
+        }
+
+        return pane;
+    }
+
+    private static Pane createTextRow(int n) {
+        HBox pane = new HBox();
+        pane.setAlignment(Pos.CENTER);
+
+        for (int i = 1; i <= n; i++) {
+            Label text = new Label();
+            text.setTextFill(Color.WHITE);
+            text.setText(String.valueOf(i));
+            pane.getChildren().add(text);
         }
 
         return pane;
