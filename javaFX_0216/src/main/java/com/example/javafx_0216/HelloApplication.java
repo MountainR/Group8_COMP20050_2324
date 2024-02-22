bc65210d9073055da6451cf2fce8eb779e7caf13
rui <112938973+MountainR@users.noreply.github.com>
2024-02-22 13:34:22 +0000

    find a way to combine fxml and javafx code

--- javaFX_0216/src/main/java/com/example/javafx_0216/HelloApplication.java
+++ javaFX_0216/src/main/java/com/example/javafx_0216/HelloApplication.java
@@ -3,6 +3,11 @@ package com.example.javafx_0216;    // package name
 import javafx.application.Application;  // the main class for JavaFX applications
 import javafx.fxml.FXMLLoader;          //  loading FXML files
 import javafx.scene.Scene;              //  for all content in a scene graph
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;              // the top-level container for a JavaFX application
 
 import java.io.IOException;
@@ -13,7 +18,18 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         // The start method is the entry point for a JavaFX application.
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+
+        StackPane rootPane = new StackPane();
+        Pane fxml = fxmlLoader.load();
+        // ---------------- I want to get coordinates from this fxml -----------------
+//        double [61][2] coordinates =
+
+
+        Pane circles = addCirclePane();
+        rootPane.getChildren().addAll(fxml, circles);
+
+        Scene scene = new Scene(rootPane, 800, 800);
+
 
         // These lines set the title of the stage, set the scene to be displayed on the stage, and then make the stage visible.
         stage.setTitle("Hello!");
@@ -21,6 +37,26 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    public Pane addCirclePane() {
+        Pane atoms = new Pane();
+        // ------- central coordinates------
+        double[][] centralCor = {
+                {10, 478},
+                {10, 10},
+                {10, 118},
+
+        };
+
+        // generate hexagons by coordinates
+        for (double[] center : centralCor) {
+            Circle atom = new Circle(center[0], center[1], 20);
+
+            // add hexagons to root
+            atom.setFill(Color.AQUA);
+            atoms.getChildren().add(atom);
+        }
+        return atoms;
+    }
     public static void main(String[] args) {
         // the entry point of the program
         launch();
