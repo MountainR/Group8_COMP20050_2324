6ac61a8397bde475aed1bc4416a5935a9475a21f
Labiba Jaigirdar <labiba.jaigirdar@ucdconnect.ie>
2024-02-22 16:59:58 +0000

    Added comments, fixed bugs

--- src/main/java/test/demo/HelloController.java
+++ src/main/java/test/demo/HelloController.java
@@ -24,26 +24,42 @@ public class HelloController {
     @FXML
     private VBox atomContainer;
 
-    @FXML
-    private Circle atom1;
-
     private ArrayList<Circle> atoms;
 
-    @FXML
-    public void displayAtoms() throws IOException {
+    private ArrayList<Circle> selectedAtoms;
+
+    public HelloController() {
 
     }
 
+    /**
+     * Displays the atoms that have been randomly selected
+     */
+    @FXML
+    public void displayAtoms() throws IOException {
+        for (int i = 0; i < selectedAtoms.size(); i++) {
+            selectedAtoms.get(i).setVisible(true); //set all atoms in arraylist to be visible
+        }
+    }
+
+    /**
+     * initializes grid with hidden atoms for every hexagon in the grid
+     */
     @FXML
     public void initialize() {
+        generateAtoms();
+        selectedAtoms = RandomlySelect.randomlySelect(atoms, 5);
+    }
+
+    /**
+     * Generates hidden atoms in each hexagon and adds them to an arraylist
+     */
+    public void generateAtoms() {
         atoms = new ArrayList<Circle>();
         int n = 5;
 
-        /*HBox row = new HBox();
-        row.setBorder(Border.stroke(Color.PINK));
-        atomContainer.getChildren().add(row);*/
-
-        for (int i = 0; i < 9; i++) {
+        //nested for loop for creation of HBoxes and atoms
+        for (int i = 0; i < 9; i++) { //loop for creation of 9 HBoxes
             HBox row = new HBox();
             row.setAlignment(Pos.TOP_CENTER);
             row.prefHeight(100);
@@ -51,19 +67,19 @@ public class HelloController {
             row.setSpacing(26);
             VBox.setMargin(row, new Insets(7, 0, 7, 0));
 
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) { //loop for creation of atoms
                 Circle atom = new Circle();
                 atom.setFill(Color.PINK);
+                atom.setVisible(false);
                 atom.setRadius(25);
                 atoms.add(atom);
-                row.getChildren().add(atom);
+                row.getChildren().add(atom); //adding atoms to HBox
             }
 
-            atomContainer.getChildren().add(row);
-            if (i < 4) n++;
-            else n--;
+            atomContainer.getChildren().add(row); //adding HBox to VBox
+            if (i < 4) n++; //number of atoms increases by 1 if the row is before the middle row
+            else n--; //number of atoms decreases by 1 if the row is after the middle row
         }
-
     }
 
 }
