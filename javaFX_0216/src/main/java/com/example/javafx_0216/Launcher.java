2cd5d10a099e373cc7f484ef900617f36f066b70
rui <112938973+MountainR@users.noreply.github.com>
2024-02-23 18:20:25 +0000

    add Launcher

---
+++ javaFX_0216/src/main/java/com/example/javafx_0216/Launcher.java
@@ -0,0 +1,10 @@
+package com.example.javafx_0216;
+
+/**
+ * Prevent jar error from too many main methods
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
--- javaFX_0216/src/main/java/module-info.java
+++ javaFX_0216/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.example.javafx_0216 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.base;
+    requires java.desktop;
 
 
     opens com.example.javafx_0216 to javafx.fxml;
